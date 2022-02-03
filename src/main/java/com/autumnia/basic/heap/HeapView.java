2666ee03a17a140120b87793476f6c3a3437dd36
Autumn Kim <autumnya@gmail.com>
2022-02-03 09:32:05 +0900

    Add files via upload
    추가

---
+++ src/main/java/com/autumnia/basic/heap/HeapView.java
@@ -0,0 +1,31 @@
+package com.autumnia.basic.heap;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
+public class HeapView {
+	private static Runtime rt = Runtime.getRuntime();
+	private static MemoryMXBean mbs = ManagementFactory.getMemoryMXBean();
+
+	public static long printHeap(int idx) {
+		rt.gc();
+		long totalMemory = rt.totalMemory();
+		long freeMemory = rt.freeMemory();
+		long usedMemory = totalMemory - freeMemory;
+		System.out.println(String.format("%d HEAP:%, 8d bytes", idx, usedMemory));
+
+		return usedMemory;
+	}
+	
+	public static void printHeap2(int count) {
+		MemoryUsage heap = mbs.getHeapMemoryUsage();
+		MemoryUsage noneHeap = mbs.getNonHeapMemoryUsage();
+		
+		System.out.println(String.format("%d  HEAP:%, NoneHeap:% TotalHeap:% ", count, heap, noneHeap ));
+	}
+
+	public static void main(String[] args)	{
+
+	}
+}
